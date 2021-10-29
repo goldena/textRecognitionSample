6eda8e40108bb659cd561c71e2061957bd4d23f3
Denis Goloborodko <goldena_by@outlook.com>
2021-10-29 16:27:45 +0300

    Initial Commit

---
+++ textRecognitionSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  textRecognitionSample
+//
+//  Created by Denis Goloborodko on 29.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
